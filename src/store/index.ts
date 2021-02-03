a2ae8102471b4bcd65d50bdd4cd939c63f73e8b4
Toan Bach <tedu.international@gmail.com>
2021-02-03 17:16:13 +0700

    remove code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,7 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 
 import { accountReducer } from './account/reducers';
 import thunkMiddleware from 'redux-thunk';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   account: accountReducer,
@@ -20,5 +12,5 @@ export type AppState = ReturnType<typeof rootReducer>;
 export default function configureStore() {
   const middlewares = [thunkMiddleware];
   const middleWareEnhancer = applyMiddleware(...middlewares);
-  return createStore(rootReducer, composeEnhancers(middleWareEnhancer));
+  return createStore(rootReducer, middleWareEnhancer);
 }
