74614cb5d1fc85f51748058b2c2770be194851ac
Toan Bach <tedu.international@gmail.com>
2021-02-22 10:28:22 +0700

    Paging

--- src/store/users/reducers.ts
+++ src/store/users/reducers.ts
@@ -10,6 +10,7 @@ const initialState: UsersState = {
   items: [],
   page: 1,
   total: 0,
+  pageSize: 0,
   loading: false,
   deletedCount: 0,
   error: null,
@@ -32,6 +33,7 @@ const usersReducer = (
         items: action.payload.items,
         total: action.payload.total,
         page: action.payload.page,
+        pageSize: action.payload.pageSize,
         loading: false,
         error: null,
       };
