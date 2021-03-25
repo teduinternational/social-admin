95c75a446636b7c0bade12f7cd4d51c39e40ba7d
Toan Bach <tedu.international@gmail.com>
2021-03-25 21:29:08 +0700

    Update socket io

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -16,6 +16,7 @@ import socketIoClient from 'socket.io-client';
 export const Admin = () => {
   const dispatch = useDispatch();
   const alert = useSelector((state: AppState) => state.alert);
+  const userId = useSelector((state: AppState) => state.account.user?._id);
   useEffect(() => {
     dispatch(getCurrentLoginUser());
   }, [dispatch]);
@@ -23,10 +24,20 @@ export const Admin = () => {
   useEffect(() => {
     const socket = socketIoClient.io(env.API_URL);
     socket.on('connect', function (data: any) {
-      console.log(data);
+      socket.emit('login', { userId: userId });
       // <-- this works
       socket.on('message', function (message: any) {
         console.log(message);
+      });
+
+      socket.on('user_created', function (message: any) {
+        console.log('Created: ' + message);
+      });
+      socket.on('user_updated', function (message: any) {
+        console.log('Updated:' + message);
+      });
+      socket.on('user_deleted', function (message: any) {
+        console.log('Deleted:' + message);
       });
     });
   });
